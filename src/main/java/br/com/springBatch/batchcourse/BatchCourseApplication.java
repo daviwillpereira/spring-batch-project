a7c69f42ea1bac3b6a6f41b9a166ff13dd59468c
daviwillpereira <daviwillpereira@gmail.com>
2021-06-12 09:21:12 -0300

    first commit

---
+++ src/main/java/br/com/springBatch/batchcourse/BatchCourseApplication.java
@@ -0,0 +1,13 @@
+package br.com.springBatch.batchcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springBatch/batchcourse/BatchCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springBatch.batchcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
