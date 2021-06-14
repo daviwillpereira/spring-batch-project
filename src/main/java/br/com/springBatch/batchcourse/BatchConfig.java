c21aa9558bcee0d1d80f42947919a78126d7bdb8
daviwillpereira <daviwillpereira@gmail.com>
2021-06-14 09:22:06 -0300

    Allowing multiple executions of a JOB, and get values through job execution parameters

--- src/main/java/br/com/springBatch/batchcourse/BatchConfig.java
+++ src/main/java/br/com/springBatch/batchcourse/BatchConfig.java
@@ -6,10 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,20 +31,27 @@ public class BatchConfig {
 		return jobBuilderFactory
 				.get("printHelloJob") //JOB's name
 				.start(printHelloStep())
+				.incrementer(new RunIdIncrementer()) // executa o JOB e incrementa um run ID a cada execuçao adicionando automaticamente um parâmetro, gerando uma nova instância do JOB. Usado quando o JOB não precisa ser reinicializado
 				.build();
 	}
 
-	private Step printHelloStep() {
+	public Step printHelloStep() {
 		return stepBuilderFactory
 				.get("printHelloStep")
-				.tasklet(new Tasklet() {    //Tasklets são usados para comandos simples, que não precisam de muito processamento
-					
-					@Override
-					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-						System.out.println("Hello, world!");
-						return RepeatStatus.FINISHED;
-					}
-				})
+				.tasklet(printHelloTaskelt(null))
 				.build();
+	}
+
+	@Bean
+	@StepScope //Permite que no momento em que estiver sendo criado o STEP, ele vai buscar o valor do jobParameters 
+	public Tasklet printHelloTaskelt(@Value("#{jobParameters['nome']}") String nome) { //O @Value permite obter dados dos parametros do JOB e tbm de propriedade do app.properties, associa o parametro no colchete com a variável do método
+		return new Tasklet() {    //Tasklets são usados para comandos simples, que não precisam de muito processamento
+			
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println(String.format("Olá, %s!", nome));
+				return RepeatStatus.FINISHED;
+			}
+		};
 	}
 }
